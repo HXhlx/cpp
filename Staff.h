8e724ab3fba4b319566008483b81eb0bb0a4de7c
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-03 18:16:50 +0800

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -1,8 +1,12 @@
 #ifndef STAFF_H
+#define STAFF_H
+#include<string>
 class Staff
 {
+	std::string wage, id, name, gender, department, occupation, phone, adress;
+	unsigned age;
 public:
 	Staff();
 	~Staff();
 };
-#endif // !STAFF_H
+#endif
