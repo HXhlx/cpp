8e03114d96323be20afdfb365059422539e71d02
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-04 18:33:54 +0800

    Update Salary.cpp

--- Salary.cpp
+++ Salary.cpp
@@ -1,7 +1,12 @@
+#include<iostream>
 #include "Salary.h"
-Salary::Salary()
+using namespace std;
+void Salary::Input()
 {
+	cin >> basic >> duty >> bonus;
 }
-Salary::~Salary()
+void Salary::Print()
 {
+	Staff::Print();
+	cout << basic << ends << duty << ends << bonus << ends << endl;
 }
