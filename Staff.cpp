23e7b70b080317ca02d211c44fbd2f84dd274e32
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-04 18:32:54 +0800

    Update Staff.cpp

--- Staff.cpp
+++ Staff.cpp
@@ -1,7 +1,16 @@
+#include<iostream>
+#include<list>
 #include "Staff.h"
-Staff::Staff()
+using namespace std;
+void Staff::Input()
 {
+	static int n;
+	cin >> id >> name >> gender >> age >> department >> occupation >> phone >> adress;
+	n++;
+	wage = 100 * 100 + n;
 }
-Staff::~Staff()
+void Staff::Print()
 {
+	cout << endl;
+	cout << wage << ends << id << ends << name << ends << gender << ends << age << ends << department << ends << occupation << ends << phone << ends << adress << ends << flush;
 }
