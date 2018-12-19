a75c2d674be2a2a622dc243cdfe88df10b8860fa
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-20 02:23:03 +0800

    Update Salary.h

--- Salary.h
+++ Salary.h
@@ -3,9 +3,15 @@
 #include "Staff.h"
 class Salary :public Staff
 {
-	int basic, duty, bonus;
+protected:
+	double basic, duty, bonus, salarysum;
 public:
+	Salary(unsigned w = 0, string i = "", string n = "", double ba = 0, double d = 0, double bo = 0) :Staff(w, i, n), basic(ba), duty(d), bonus(bo) 
+	{
+		salarysum = basic + duty + bonus;
+	}
 	void Input();
 	void Print();
+	void Modify();
 };
 #endif
