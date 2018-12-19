df183eeace202d4c719321e34f400413b1a6349a
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-20 02:26:43 +0800

    Update Charge.h

--- Charge.h
+++ Charge.h
@@ -1,11 +1,20 @@
 #ifndef CHARGE_H
-#include"Salary.h"
 #define CHARGE_H
+#include"Salary.h"
+using namespace std;
 class Charge :public Salary
 {
-	int water, electricity, medicare;
+	double water, electricity, medicare, rax, realsalary;
 public:
-	Charge();
-	~Charge();
+	Charge(unsigned w = 0, string i = "", string n = "", double ba = 0, double d = 0, double bo = 0, double wa = 0, double e = 0, double m = 0) :Salary(w, i, n, ba, d, bo), water(wa), electricity(e), medicare(m) {}
+	void BasicIn();
+	static int Login(vector<Charge>&);
+	static void In(fstream&, vector<Charge>&);
+	static void Save(fstream&, vector<Charge>&);
+	void feagure();
+	void Input();
+	void Print();
+	void Modify();
+	void Delete(vector<Charge>&);
 };
 #endif
