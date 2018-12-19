b5cdce51e7374e8fcc40498cd826086c8f5488ce
HXhlx <44925573+HXhlx@users.noreply.github.com>
2018-12-20 02:27:56 +0800

    Update Salary.cpp

--- Salary.cpp
+++ Salary.cpp
@@ -1,12 +1,24 @@
+#include<algorithm>
 #include<iostream>
+#include<vector>
 #include "Salary.h"
 using namespace std;
 void Salary::Input()
 {
-	cin >> basic >> duty >> bonus;
+	if (!basic&&!duty&&!bonus)
+	{
+		Staff::Print();
+		cin >> basic >> duty >> bonus;
+	}
 }
 void Salary::Print()
 {
-	Staff::Print();
-	cout << basic << ends << duty << ends << bonus << ends << endl;
+	salarysum = basic + duty + bonus;
+	cout<<"工资卡号:"<<wage<<ends<<"姓名:"<<name<<ends<<"应发工资:"<<salarysum<<endl;
+}
+void Salary::Modify()
+{
+	cout << "请输入新的员工工资信息：基本工资，职务工资，特别奖励" << endl;
+	cin >> basic >> duty >> bonus;
+	cout << "成功修改员工工资信息" << endl;
 }
