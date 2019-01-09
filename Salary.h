c2d4c115708ba49f7cc843aca1f5a643e29b6379
HXhlx <44925573+HXhlx@users.noreply.github.com>
2019-01-09 22:46:37 +0800

    Update Salary.h

--- Salary.h
+++ Salary.h
@@ -6,12 +6,10 @@ class Salary :public Staff
 protected:
 	double basic, duty, bonus, salarysum;
 public:
-	Salary(unsigned w = 0, string i = "", string n = "", double ba = 0, double d = 0, double bo = 0) :Staff(w, i, n), basic(ba), duty(d), bonus(bo) 
-	{
-		salarysum = basic + duty + bonus;
-	}
-	void Input();
-	void Print();
-	void Modify();
+	Salary(unsigned w = 0, string i = "", string n = "", double ba = 0, double d = 0, double bo = 0) :Staff(w, i, n), basic(ba), duty(d), bonus(bo) { salarysum = basic + duty + bonus; }
+	static void Find(vector<Salary>);		/*按工资信息查询*/
+	void Input();							/*添加员工工资信息*/
+	void Modify();							/*修改员工工资信息*/
+	void Print();							/*工资信息查询*/
 };
 #endif
