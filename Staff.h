49ec6626e7f5a8b238122dad9e7ba11ddfc8f8cf
HXhlx <44925573+HXhlx@users.noreply.github.com>
2019-01-09 22:45:04 +0800

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -2,7 +2,7 @@
 #define STAFF_H
 #include<string>
 #include<vector>
-using std::string;
+using namespace std;
 class Staff
 {
 	friend class Administrator;
@@ -12,9 +12,11 @@ protected:
 	unsigned wage, age;
 public:
 	Staff(unsigned w = 0, string i = "", string n = "") :wage(w), id(i), name(n) {}
-	void Input(Staff);
-	void Print();
-	void Change();
-	void Modify();
+	static void Find(vector<Staff>);		/*按基本信息查询*/
+	void BasicIn();							/*基本输入*/
+	void Input(Staff);						/*添加员工基本信息*/
+	void Print();							/*基本信息查询*/
+	void Change();							/*修改密码*/
+	void Modify();							/*员工基本信息修改*/
 };
 #endif
