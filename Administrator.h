bf75d7309bd28288d58ad7c11d816e24b93b678c
HXhlx <44925573+HXhlx@users.noreply.github.com>
2019-01-09 22:41:50 +0800

    Update Administrator.h

--- Administrator.h
+++ Administrator.h
@@ -6,17 +6,16 @@
 using namespace std;
 class Administrator
 {
-	friend bool operator==(Administrator A, Administrator B);
+	friend bool operator==(Administrator, Administrator);
 	string name, password;
 public:
 	Administrator(string n = "", string p = "") :name(n), password(p) {}
-	static void In(fstream&, list<Administrator>&);
-	void Input(list<Administrator>&);
-	static Administrator Login(list<Administrator>);
-	friend bool operator==(Administrator A, Administrator B);
-	void Change(list<Administrator>&);
-	bool Delete(list<Administrator>&);
-	static void Save(fstream&, list<Administrator>&);
-	void ModifyStaff(Staff&);
+	static void In(fstream&, list<Administrator>&);				/*将管理员文件输出到链表中*/
+	static Administrator Login(list<Administrator>);			/*管理员登录*/
+	static void Save(fstream&, list<Administrator>&);			/*保存到文件中*/
+	void Change(list<Administrator>&);					/*修改密码*/
+	bool Delete(list<Administrator>&);					/*删除管理员信息*/
+	void Input(list<Administrator>&);					/*添加管理员信息*/
+	void ModifyStaff(Staff&);						/*修改员工基本信息*/
 };
 #endif
